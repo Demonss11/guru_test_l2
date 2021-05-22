4f6b82dad9cba4b41cf05a9b370066a382723e1d
Belousov Dmitry <demonss11@gmail.com>
2021-05-22 20:36:12 +0300

    working version2

---
+++ src/test/java/tests/base/TestBase.java
@@ -0,0 +1,12 @@
+package tests.base;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import com.codeborne.selenide.Configuration;
+
+public class TestBase {
+    @BeforeAll
+    static void setup(){
+        Configuration.startMaximized = true;
+    }
+}
